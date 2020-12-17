6f905d3e8d5180fb10197bb393762c6c16a356b3
Glitch (savory-curvy-oxygen) <none>
2020-12-17 16:32:44 +0000

    Age calculator by danielle

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+let birthYear= null;
+let age= null;
+let name = null;
+let currentYear = null;
+
+
+$("button").click(function() {
+  console.log('year', birthYear)
+  console.log('age', age)
+  birthYear =$(".year").val();
+  console.log('year', birthYear)
+  currentYear = $(".current-year").val();
+  age = (currentYear - birthYear)
+  console.log('age:', age)
+  name = $(".name").val();
+  $(".message").html(`${name} is ${age} cool`); });
